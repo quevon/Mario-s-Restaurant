8b786425fa07efed0440e517f0ca279e794e30e4
Quevon Montojo Galang <91044904+quevon@users.noreply.github.com>
2023-01-15 12:50:34 -0800

    added transition on scroll up and down

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,10 +1,14 @@
 export const scroll = () => {
   window.addEventListener("scroll", () => {
+    const homeContent = document.querySelector(".home-content");
     const abountContent = document.querySelector(".about-content");
     const menus = document.querySelector(".menu-content");
     const events = document.querySelector(".events-content");
     const contact = document.querySelector(".contact-content");
     const rooms = document.querySelector(".rooms-content");
+
+    const homeContentPosition = homeContent.getBoundingClientRect().top;
+    const homePosition = window.innerHeight / 1;
 
     const abountContentPosition = abountContent.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1;
@@ -21,28 +25,48 @@ export const scroll = () => {
     const roomsContentPosition = rooms.getBoundingClientRect().top;
     const roomsPosition = window.innerHeight / 1;
 
+    if (homeContentPosition < homePosition) {
+      homeContent.classList.add("active-home");
+    } else {
+      homeContent.classList.remove("active-home");
+    }
     if (abountContentPosition < screenPosition) {
       abountContent.classList.add("active");
+      homeContent.classList.remove("active-home");
     } else {
       abountContent.classList.remove("active");
     }
     if (menusContentPosition < menusPosition) {
       menus.classList.add("active-menus");
+      homeContent.classList.remove("active-home");
+      abountContent.classList.remove("active");
     } else {
       menus.classList.remove("active-menus");
     }
     if (eventsContentPosition < eventsPosition) {
       events.classList.add("active-events");
+      menus.classList.remove("active-menus");
+      homeContent.classList.remove("active-home");
+      abountContent.classList.remove("active");
     } else {
       events.classList.remove("active-events");
     }
     if (contactContentPosition < contactPosition) {
       contact.classList.add("contact-events");
+      menus.classList.remove("active-menus");
+      homeContent.classList.remove("active-home");
+      abountContent.classList.remove("active");
+      events.classList.remove("active-events");
     } else {
       contact.classList.remove("contact-events");
     }
     if (roomsContentPosition < roomsPosition) {
       rooms.classList.add("rooms-events");
+      menus.classList.remove("active-menus");
+      homeContent.classList.remove("active-home");
+      abountContent.classList.remove("active");
+      events.classList.remove("active-events");
+      contact.classList.remove("contact-events");
     } else {
       rooms.classList.remove("rooms-events");
     }
