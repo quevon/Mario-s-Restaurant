48fb5294c8ed437ce6c423acea0e9aaff9ed2669
Quevon Montojo Galang <91044904+quevon@users.noreply.github.com>
2023-01-28 16:40:04 -0800

    added testimonial content

--- script.js
+++ script.js
@@ -58,4 +58,25 @@ $(document).ready(function () {
       },
     },
   });
+  $(".carousel2").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 1,
+        nav: false,
+      },
+    },
+  });
 });
